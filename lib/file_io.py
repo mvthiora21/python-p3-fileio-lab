a33a5c9e708fc0b629c95ffde9f2266722e7efd3
victormuthiora <victormuthiora617@gmail.com>
2024-06-04 09:53:29 +0300

    finished

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,14 @@
-def write_file(file_name, file_content):
-    pass
 
-def append_file(file_name, append_content):
-    pass
+
+def write_file(file_name, file_content):
+    with open(str(file_name) + ".txt",mode="w") as file:
+        file.write(file_content)
+
+def append_file(file_name, file_content):
+    with open(str(file_name) + ".txt", mode="a") as file:
+        file.write(file_content)
 
 def read_file(file_name):
-    pass
+    with open(str(file_name) + ".txt", mode="r") as file:
+        content = file.read()
+    return content
